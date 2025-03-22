2fa98c0ca32414a65a2e8210fea4f90abcde7d03
Krishna <krishnaag866@gmail.com>
2025-03-22 14:15:17 +0530

    launch website from logo

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title='Hello from BridgeLabz'
   logoUrl= 'assets/images/bridgelabz_com_logo.jpg';
+  websiteUrl= 'https://www.bridgelabz.com';
 }
