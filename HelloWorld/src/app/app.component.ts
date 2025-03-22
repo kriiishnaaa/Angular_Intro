2f59d10cc66bcb31dc8d672edf4d92091bbfb1d1
Krishna <krishnaag866@gmail.com>
2025-03-22 15:08:44 +0530

    Username input with regex

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone:true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   userName='';
+  errorMessage='';
   title='Hello from BridgeLabz';
   logoUrl= 'assets/images/bridgelabz_com_logo.jpg';
   websiteUrl= 'https://www.bridgelabz.com';
+  isValid=true;
+
   onInputChange(event: Event) {
-    this.userName = (event.target as HTMLInputElement).value;
+    const input = (event.target as HTMLInputElement).value;
+    this.userName = input;
+    this.isValid = /^[A-Z][a-zA-Z]{2,}$/.test(input); // Must start with uppercase and be at least 3 characters
   }
 }
+
