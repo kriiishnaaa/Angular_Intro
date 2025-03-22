e5be6053660ef0cf255f9b81db7873a9ea3ab4aa
Krishna <krishnaag866@gmail.com>
2025-03-22 12:14:58 +0530

    Hello World using Data Binding

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  message: string = "Hello from BridgeLabz"; // Property to bind using interpolation
 }
